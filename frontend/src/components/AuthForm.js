3abbefe5c335994e47ceb68d6f2a07cd8be7e652
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-09-04 12:54:48 +0530

    added useSearchParams

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,14 +1,17 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+//import { useState } from 'react';
+import { Form ,useSearchParams, Link} from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [searchParams] = useSearchParams();
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const isLogin = searchParams.get('mode') === "login";
+  // const [isLogin, setIsLogin] = useState(true);
+
+  // function switchAuthHandler() {
+  //   setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
+  // }
 
   return (
     <>
@@ -23,9 +26,12 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          {/* <button onClick={switchAuthHandler} type="button">
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          </button> */}
+          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
+            {isLogin ? 'Create new user' : 'Login'}
+          </Link>
           <button>Save</button>
         </div>
       </Form>
